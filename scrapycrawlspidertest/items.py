7a0685bfb3a768b918765f1f580ba49dda96c33b
Germey <cqc@cuiqingcai.com>
2017-08-10 20:00:32 +0800

    update config

--- scrapycrawlspidertest/items.py
+++ scrapycrawlspidertest/items.py
@@ -5,10 +5,13 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Field, Item
 
 
-class ScrapycrawlspidertestItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class NewsItem(Item):
+    title = Field()
+    text = Field()
+    datetime = Field()
+    source = Field()
+    url = Field()
+    website = Field()
